c8d5da184174d14a5871687cbbc1549ebeec710f
Anne Kelly <annekellyc@gmail.com>
2015-07-03 14:38:57 -0300

    Anne - command to remove jar on install.sh.

--- infrastructure/scripts/aws/install.sh
+++ infrastructure/scripts/aws/install.sh
@@ -3,17 +3,20 @@
 echo "=====> Stopping hackerbrasileiro service"
 ssh -i /var/go/hackerbrasileiro-key.pem ubuntu@52.11.131.93 "service hackerbrasileiro stop"
 
-echo "=====> Removing generate.py file"
-ssh -i /var/go/hackerbrasileiro-key.pem ubuntu@52.11.131.93 "rm -rf facemorpher/generate.py"
-
-echo "=====> Adding generate.py file"
-scp -i /var/go/hackerbrasileiro-key.pem facemorpher/generate.py ubuntu@52.11.131.93:app/facemorpher/
+echo "=====> Stopping hackerbrasileiro jar"
+ssh -i /var/go/hackerbrasileiro-key.pem ubuntu@52.11.131.93 "rm -rf app/hackerbrasileiro.jar"
 
 echo "=====> Generate jar"
 ./gradlew hackerbrasileiroJar
 
 echo "=====> Copy jar to app folder"
 scp -i /var/go/hackerbrasileiro-key.pem hackerbrasileiro.jar ubuntu@52.11.131.93:app/
+
+echo "=====> Removing generate.py file"
+ssh -i /var/go/hackerbrasileiro-key.pem ubuntu@52.11.131.93 "rm -rf facemorpher/generate.py"
+
+echo "=====> Adding generate.py file"
+scp -i /var/go/hackerbrasileiro-key.pem facemorpher/generate.py ubuntu@52.11.131.93:app/facemorpher/
 
 echo "=====> Starting hackerbrasileiro service"
 ssh -i /var/go/hackerbrasileiro-key.pem ubuntu@52.11.131.93 "service hackerbrasileiro start"
